68a3b768d3b354f7186b60e227a6e0464a958eac
Evyde <life.app.hanfeng@Hotmail.com>
2019-11-19 20:20:25 +0800

    Add README.md and setup.py to test if the git works.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DONOTKNOW',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='韩枫',
+    author_email='hanfeng2419@jlu.edu.cn',
+    description=''
+)
